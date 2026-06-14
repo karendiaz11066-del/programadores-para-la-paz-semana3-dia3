d6b02bf4f30920f37e024e4882580278e5021726
karendiaz11066-del <karendiaz11066@gmail.com>
2026-06-14 02:29:26 +0000

    Activida 3

--- ciclo-while.js
+++ ciclo-while.js
@@ -1,12 +1,12 @@
 let intento = 1
-const maximoIntentos = 3
+const maximoIntentos = 6
 
 console.log("Práctica inicial con while")
 console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al máximo.")
 
 while (intento <= maximoIntentos) {
   console.log("Intento de revisión número:", intento)
-  intento = intento + 1
+  intento = intento + 3
 }
 
 console.log("Proceso de revisión finalizado.")
---
+++ seguimiento-comunitario.js
@@ -0,0 +1,18 @@
+let semana = 1
+const semanaDeseguimiento = 4
+let propuestaAtendidas = 0
+
+console.log ("seguimiento comunitario semanal")
+
+while (semanaDeseguimiento) {
+    console.log("semana de seguimientos numeros:",)
+    propuestaAtendidas = propuestaAtendidas =2
+    console.log("propuestas atendidas hasta ahora:",propuestaAtendidas semana = semana + 1
+    
+
+     )console.log("seguimiento comunitario finalizado.")
+     console.log("total de propuestas atendidas:",propuestaAtendidas)
+
+}
+
+
